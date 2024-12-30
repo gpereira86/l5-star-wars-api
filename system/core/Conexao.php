ad3352a300b77db2fc742589f34226f2e940da3d
Glauco Pereira <glaucoadm@gmail.com>
2024-12-30 10:50:49 -0300

    L5-Initial commit

---
+++ system/core/Conexao.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace system\core;
+
+use PDO;
+use PDOException;
+
+/**
+ * Conexão com o Banco de Dados
+ *
+ * @author Glauco Pereira <eu@glaucopereira.com>
+ * @copyright Copyright (c) 2024, Glauco Pereira
+ */
+class Conexao
+{
+
+    private static $instancia;
+
+    /**
+     * Conexão PDO Banco MySql usando constantes de sistema do arquivo de configurações
+     *
+     * @return PDO
+     */
+    public static function getInstancia(): PDO
+    {
+        if (empty(self::$instancia)) {
+
+            try {
+
+                self::$instancia = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORTA . ';dbname=' . DB_NOME, DB_USUARIO, DB_SENHA, [
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "set NAMES utf8",
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                    PDO::ATTR_CASE => PDO::CASE_NATURAL
+                ]);
+            } catch (PDOException $ex) {
+
+                die("Erro de Conexão >>> " . $ex->getMessage());
+            }
+        }
+        return self::$instancia;
+    }
+
+    /**
+     * Contrutor (proteção)
+     */
+    protected function __construct()
+    {
+
+    }
+
+    /**
+     * Clone (proteção)
+     */
+    private function __clone(): void
+    {
+
+    }
+
+}
---
+++ system/core/helpers.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace helpers;
+
+use helpers\helpers\string1;
+use const helpers\helpers\URL_DESENVOLVIMENTO;
+use const helpers\helpers\URL_PRODUCAO;
+
+class Helpers
+{
+    /**
+     *  Retorna Verdadeiro ou falso para ambiente localhost
+     *
+     * @return bool
+     */
+    public static function localhost(): bool
+    {
+        $server = filter_input(INPUT_SERVER, 'SERVER_NAME');
+
+        if ($server == 'localhost') {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Monta URL de acordo com Ambiente desenvolvimento ou produção
+     *
+     * @param string|null $url
+     * @return string1
+     */
+    public static function url(string $url = null): string
+    {
+        $server = filter_input(INPUT_SERVER, 'SERVER_NAME');
+        $environmentInUse = ($server == 'localhost' ? URL_DESENVOLVIMENTO : URL_PRODUCAO);
+
+        if (str_starts_with($url, '/')) {
+            return$environmentInUse . $url;
+        }
+
+        return$environmentInUse . '/' . $url;
+    }
+
+
+    /**
+     * Redirecionamento de URL simplidicado, apenas com o slug
+     *
+     * @param string|null $url
+     * @return void
+     */
+    public static function redirectUrl(string $url = null): void
+    {
+        header('HTTP/1.1 302 found');
+
+        $local = ($url ? self::url($url) : self::url());
+
+        header("location: {$local}");
+        exit();
+    }
+
+}
